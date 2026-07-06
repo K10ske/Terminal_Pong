0bad93f39187bc2aa407ad06c76723d229abc1e6
K10ske <beltoforion04@protonmail.com>
2026-07-06 20:26:03 +0800

    added int main to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@ using namespace std;
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 void destroy_win(WINDOW *local_win);
 
+int main(){
+	initscr();
+
+	endwin();
+}
+
 WINDOW *create_newwin(int height, int width, int starty, int startx)
 {	WINDOW *local_win;
 
